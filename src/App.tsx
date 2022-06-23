45f30f9d0e970dfc2bb270cee3b4ced840649b93
Adrian-007391 <101372036+Adrian-007391@users.noreply.github.com>
2022-06-23 14:49:54 -0500

    css mods

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import {useState}from 'react';
 import './App.css';
-import { getValue } from '@testing-library/user-event/dist/utils';
 
 function App() {
     const [html , sethtml] = useState<string>(" "); 
+    const [mdt , setmdt] = useState<string>("");
+    
     var showdown  = require('showdown');
      let converter = new showdown.Converter();
     function addtext(ntext:string){
+      setmdt(ntext);
        sethtml(converter.makeHtml(ntext));
+    }
+    function savetext(){
+      var a = document.createElement("a");
+      let name = prompt("nombre del archivo")
+      a.href = window.URL.createObjectURL(new Blob([mdt], {type: "markdown"}));
+      a.download = name+".md";
+      a.click();
     }
   return (
     <div className="App">
      <textarea className='App__text' onChange={e=>{addtext(e.target.value)}}/>
      <div className="App__div--markdowndisplay" dangerouslySetInnerHTML={{__html: html}}></div> 
+     <button className='App__button--save' onClick={()=>{savetext()}}>save md file</button>
+
     </div>
   );
 }
